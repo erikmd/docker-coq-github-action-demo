d4dc603d696ebf0b46fae5b2dd767e40a1650ae6
Erik Martin-Dorel <erik@martin-dorel.org>
2024-07-14 22:32:03 +0200

    fix: Remove ("Variable A: Prop", identity lemma)
    as it now raises:
    
    ```
    File "./src/demo.v", line 16, characters 0-18:
    Error: Use of "Variable" or "Hypothesis" outside sections behaves as
    "#[local] Parameter" or "#[local] Axiom".
    [declaration-outside-section,vernacular,default]
    ```
    
    & Update CI test suite

--- src/demo.v
+++ src/demo.v
@@ -11,14 +11,6 @@ idtac "proof in progress...".
 now intros P [H1 H2]; apply H1; apply H2; intros HP; apply H1.
 Qed.
 
-Print thm.
-
-Variable A : Prop. (* raise a warning *)
-Lemma identity : A -> A.
-Proof.
-easy.
-Qed.
-
 Lemma eqs : forall a b c : nat, (a, b) = (b, a) -> b = c -> a = c.
 move=> a b c Hab Hc.
 case: Hab=>->_.
